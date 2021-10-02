9c04f53c54a8f6d7ff72748f4ff0c1b795178315
Ben Garfield <bengarfield@gmail.com>
2021-10-01 22:09:27 -0400

    include the whole templates directory
    - rather than forcing ourselves to include
    each template by hand, this setup includes
    the whole folder in more useful ways
    - "LoadHTMLGlob" lets us dictate a pattern
    relative to where we're running from that
    we'll look for matching html files from,
    supporting iteration on those assets without
    needing to re-build the Go binary
    - the embed approach loads in the same files,
    but does so from the version of the templates
    directory that was baked into our binary. this
    lets us ship our server and its assets as a
    single artifact

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"embed"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLFiles("templates/index.gohtml")
+	// if true, read live files from local filesystem instead of embedded assets
+	useLiveFiles := gin.Mode() != gin.ReleaseMode
+	loadTemplates(r, useLiveFiles)
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.gohtml", gin.H{
 			"title": "GOAuth Index",
@@ -17,4 +22,19 @@ func main() {
 	if err := r.Run(":80"); err != nil {
 		log.Fatal(err)
 	}
+}
+
+//go:embed templates
+var htmlTemplates  embed.FS
+
+func loadTemplates(r *gin.Engine, useLiveFiles bool) {
+	if useLiveFiles {
+		r.LoadHTMLGlob("templates/*")
+		return
+	}
+	t, err := template.ParseFS(htmlTemplates,  "templates/*")
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse embedded templates: %v", err))
+	}
+	r.SetHTMLTemplate(t)
 }
