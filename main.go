0303e21e64b3e7041032f6e5cde2969f22477684
Ben Garfield <bengarfield@gmail.com>
2021-10-01 21:51:32 -0400

    Create an index page for GOAuth
    - This will give us a place to drop in the different
    OAuth sign-in buttons for users to choose from, and
    introduces us to some of the convenient gin helpers
    - "LoadHTMLFiles" is a super simple way for us to make
    an enumerated list of HTML templates available for
    easy response rendering, we'll improve on this later
    - c.HTML is a shorthand response handler that lets  us
    provide a status, a template name, and template parameters
    - gin.H is an alias for map[string]string, which gives us
    a simple way to provide template parameters

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLFiles("templates/index.gohtml")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.gohtml", gin.H{
+			"title": "GOAuth Index",
+		})
+	})
 	if err := r.Run(":80"); err != nil {
 		log.Fatal(err)
 	}
